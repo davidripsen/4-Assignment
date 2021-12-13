e13b6aa7828a880a418786d4aa55554f151462ed
DavidRIpsen <s164522@student.dtu.dk>
2021-12-13 13:37:07 +0100

    Init2

---
+++ Modelling.R
@@ -0,0 +1,8 @@
+# 4. Assignment - Wind
+
+if (Sys.info()[7] == "davidipsen"){
+  setwd("~/Documents/DTU/3. Semester (MSc)/Advanced Time Series/Assignments/4-Assignment/comp_ex_4_scripts_2011/r/")
+}
+D = read.table("../data/cex4WindDataInterpolated.csv")
+D
+D
